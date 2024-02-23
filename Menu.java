0f5b5997d13e33b3acdab72dff3a9bfae163cee2
TaniaRizwan <105510572+TaniaRizwan@users.noreply.github.com>
2024-02-22 20:38:45 -0700

    1. Added Main.java and Test.java classes 2. Setup the menu

--- Menu.java
+++ Menu.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+/** This class interacts with the user to get inputs. It displays the menu of the habit tracker.
+ * Names: Tania Rizwan, Sanbeer Shafin, Phone Myat
+ * Tutorial:(fill)
+ */
+
 public class Menu {
+
+    // Create scanner object for user input.
+        // Private so it is only accessible within the Menu class.
+        // Static because everything in this class can share the scanner object.
+    private static final Scanner scanner = new Scanner(System.in);
+
+    // Create menu for user interaction.
+    // Use a static initializer. This initializes the ArrayList when we load the Menu class.
+    private static final ArrayList<String> options = new ArrayList<>();
+    static {
+        options.add("Enter your name: "); // At index 0
+    }
+
+
+    // Create message to display to user. Then use static initializer to combine message and Options into one menu.
+    private static String optMessage = """
+            Welcome to your personal habit and goal tracker!
+            Enter and track your habits for the week!
+            \t Menu Options:
+            """;
+    static {
+        StringBuilder sb = new StringBuilder();
+        sb.append(optMessage);
+        for (int i = 0; i < options.size(); i++) {
+            sb.append(String.format("\t%d. %s\n", i, options.get(i) )); // Append the index and menu item
+        }
+        optMessage = sb.toString(); // Update the optMessage object with the final menu
+    }
+
+
+    public static void menuLoop() {
+        System.out.print(optMessage);
+    }
 }
