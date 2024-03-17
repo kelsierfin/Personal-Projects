2885b3cba259e1ebf6914bf56e3ca32b5482b867
TaniaRizwan <105510572+TaniaRizwan@users.noreply.github.com>
2024-03-17 12:56:35 -0600

    Implemented Goal.java class. Populated it with the private fields, constructor, getters and setters.

--- src/core/objects/Goal.java
+++ src/core/objects/Goal.java
@@ -1,4 +1,42 @@
 package core.objects;
 
 public class Goal {
+
+    // Object fields
+    private String goal;
+    private Integer idealCount;
+    private String category;
+
+    // Constructor
+    public Goal (String goal, Integer idealCount, String category){
+        this.goal = goal;
+        this.idealCount = 0;
+        this.category = category;
+    }
+
+    // Getters
+    public String getGoal(){
+        return this.goal;
+    }
+
+    public Integer getIdealCount(){
+        return this.idealCount;
+    }
+
+    public String getCategory(){
+        return this.category;
+    }
+
+    // Setters
+    public void setGoal(String goalName){
+        this.goal = goalName;
+    }
+
+    public void setIdealCount(Integer idealCount) {
+        this.idealCount = idealCount;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
 }
