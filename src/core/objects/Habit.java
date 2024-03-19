10e43c66b64fc82942a16aafb0b135dae6b357be
TaniaRizwan <105510572+TaniaRizwan@users.noreply.github.com>
2024-03-18 19:44:11 -0600

    Updated habit.java with fields, getters and setters.
    Updated menu.java (creategoal method) and data,java creategoal method

---
+++ src/core/objects/Habit.java
@@ -0,0 +1,52 @@
+package core.objects;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Habit extends Goal{
+
+    private String habit;
+
+    private Integer currentCount;
+
+    private ArrayList<String> habitsList;
+
+//    private ArrayList<String> habitsList;
+
+//    private HashMap<Goal, ArrayList<>>
+
+    public Habit(String goal, Integer idealCount, String category, Integer currentCount,String habit){
+        super(goal, idealCount, category);
+        this.habit = habit;
+        this.currentCount = currentCount;
+    }
+
+    public String getHabit() {
+        return habit;
+    }
+
+    public void setHabit(String habit) {
+        this.habit = habit;
+    }
+
+    public Integer getCurrentCount() {
+        return currentCount;
+    }
+
+    public void setCurrentCount(Integer currentCount) {
+        this.currentCount = currentCount;
+    }
+
+    @Override
+    public String toString() {
+        return "HabitsList{" +
+                "habit='" + habit + '\'' +
+                ", currentCount=" + currentCount +
+                '}';
+    }
+
+
+
+
+
+}
